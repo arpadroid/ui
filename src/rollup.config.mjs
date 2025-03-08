4ec389a78ea1c00f1ed5134d08d56f5c32d8a402
Andres Vaquero <web.andresvaquero@gmail.com>
2025-03-09 00:41:34 +1100

    removing ts ignores

--- src/rollup.config.mjs
+++ src/rollup.config.mjs
@@ -1,10 +1,9 @@
-// @ts-ignore
-import { getBuild, rollupCopy } from '@arpadroid/module';
-const { build = {}, appBuild } = getBuild('ui', 'uiComponent');
+import { getBuild } from '@arpadroid/module';
+const { build = {}, appBuild, Plugins } = getBuild('ui', 'uiComponent');
 appBuild.plugins = appBuild.plugins || [];
 Array.isArray(appBuild.plugins) &&
     appBuild.plugins?.push(
-        rollupCopy({
+        Plugins.copy({
             targets: [
                 { src: 'node_modules/material-symbols', dest: 'dist' },
                 { src: 'src/demo.html', dest: 'dist' }
