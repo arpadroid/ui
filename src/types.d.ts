91b8ba7d2772acdd291dfcbdd6933b89a2901459
Andres Vaquero <web.andresvaquero@gmail.com>
2026-05-29 00:45:28 +1000

    updating manifest

--- src/types.d.ts
+++ src/types.d.ts
@@ -12,7 +12,7 @@ export * from './components/inputCombo/inputCombo.types';
 export * from './components/pager/pager.types';
 export * from './components/pager/components/pagerItem/pagerItem.types';
 export * from './components/buttons/button/button.types';
-export * from './components/iconButton/iconButton.types';
+export * from './components/buttons/iconButton/iconButton.types';
 export * from './components/dialogs/dialog/dialog.types';
 export * from './components/dropArea/dropArea.types';
 export * from './components/image/image.types';
